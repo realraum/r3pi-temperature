89adb1fcff186ab18fbe3a78e3b1d73f1e82d643
stefan <dev@2904.cc>
2015-08-04 01:41:56 +0200

    switched temperature measurement from sensor to spaceapi

--- r3temp.py
+++ r3temp.py
@@ -1,3 +1,7 @@
+import requests
+import json
+
+
 def loadTemp():
     tfile = open("/sys/bus/w1/devices/10-000801375be4/w1_slave")
     text = tfile.read()
@@ -8,9 +12,20 @@ def loadTemp():
     temperature = temperature / 1000
     return temperature
 
-def getTemp():
+
+def getTempFromSensor():
     while True:
         temp = loadTemp()
         if temp < 85:
             break
     return temp
+
+
+def getTempByName(data, name):
+    return filter(lambda x: x['name'] == name, data['sensors']['temperature'])[0]['value']
+
+
+def getTemp():
+    resp = requests.get(url="http://realraum.at/status.json")
+    data = resp.json()
+    return getTempByName(data, 'Temp1')
